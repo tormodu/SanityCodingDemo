f12cbc81f6438ccb98d7a8522329007af28694d8
Eivind Christensen <eivind.ch@gmail.com>
2021-09-27 15:20:22 +0200

    Check if build

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,16 +1,16 @@
-import Head from 'next/head'
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { CMS_NAME } from '../lib/constants'
-import { indexQuery } from '../lib/queries'
-import { getClient, overlayDrafts } from '../lib/sanity.server'
+import Head from "next/head";
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { CMS_NAME } from "../lib/constants";
+import { indexQuery } from "../lib/queries";
+import { getClient, overlayDrafts } from "../lib/sanity.server";
 
 export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout preview={preview}>
@@ -19,6 +19,7 @@ export default function Index({ allPosts, preview }) {
         </Head>
         <Container>
           <Intro />
+          <h1>HER LA JEG INN NOE</h1>
           {heroPost && (
             <HeroPost
               title={heroPost.title}
@@ -33,12 +34,12 @@ export default function Index({ allPosts, preview }) {
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery))
+  const allPosts = overlayDrafts(await getClient(preview).fetch(indexQuery));
   return {
     props: { allPosts, preview },
-  }
+  };
 }
